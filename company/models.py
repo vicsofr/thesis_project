ad3c5d9c986d3701685eeb131fc2559621d6dc7e
Lev Sentsov <l.sentsov@napoleonit.com>
2023-05-23 19:54:46 +0500

    1) added Pillow to req-s.txt 2) added models for Employee and Department 3) added model to admin panel

--- company/models.py
+++ company/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Department(models.Model):
+    name = models.CharField(max_length=100)
+    director = models.OneToOneField('Employee', on_delete=models.SET_NULL, null=True, related_name='department_dir')
+
+
+class Employee(models.Model):
+    full_name = models.CharField(max_length=100)
+    photo = models.ImageField(upload_to='employee_photos')
+    position = models.CharField(max_length=100)
+    salary = models.DecimalField(max_digits=10, decimal_places=2)
+    age = models.IntegerField()
+    department = models.ForeignKey(Department, on_delete=models.CASCADE, related_name='employees')
