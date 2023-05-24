e9cfcb209745bdfb7eaba4fc4d38ba1ef2b02711
Lev Sentsov <l.sentsov@napoleonit.com>
2023-05-24 11:41:47 +0500

    change model, changed requirements, add migration

--- company/models.py
+++ company/models.py
@@ -5,11 +5,17 @@ class Department(models.Model):
     name = models.CharField(max_length=100)
     director = models.OneToOneField('Employee', on_delete=models.SET_NULL, null=True, related_name='department_dir')
 
+    def __str__(self):
+        return f'Департамент "{self.name}"'
+
 
 class Employee(models.Model):
-    full_name = models.CharField(max_length=100)
-    photo = models.ImageField(upload_to='employee_photos')
+    full_name = models.CharField(max_length=100, db_index=True)
+    photo = models.ImageField(upload_to='employee_photos', blank=True)
     position = models.CharField(max_length=100)
     salary = models.DecimalField(max_digits=10, decimal_places=2)
-    age = models.IntegerField()
+    age = models.IntegerField(max_length=3)
     department = models.ForeignKey(Department, on_delete=models.CASCADE, related_name='employees')
+
+    def __str__(self):
+        return self.full_name
